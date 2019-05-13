ace3c83aaf1a7d4d182a74d4da59d603614eeb8c
DG El Matador <daringarcia@gmail.com>
2019-05-13 00:43:31 -0400

    added files

---
+++ app/data/friends.js
@@ -0,0 +1,64 @@
+var friends = [
+    {
+        name: 'Olga Kurylenko',
+        photo:
+            'https://vignette.wikia.nocookie.net/james-bond-007/images/f/fe/Olga_kurylenko.jpg/revision/latest?cb=20130928174353',
+        scores: [5, 1, 4, 4, 1, 2, 5, 4, 4]    
+    },
+    {
+        name: 'Olga Kurylenko',
+        photo:
+            'https://vignette.wikia.nocookie.net/james-bond-007/images/f/fe/Olga_kurylenko.jpg/revision/latest?cb=20130928174353',
+        scores: [5, 1, 4, 4, 1, 2, 5, 4, 4]    
+    },
+    {
+        name: 'Olga Kurylenko',
+        photo:
+            'https://vignette.wikia.nocookie.net/james-bond-007/images/f/fe/Olga_kurylenko.jpg/revision/latest?cb=20130928174353',
+        scores: [5, 1, 4, 4, 1, 2, 5, 4, 4]    
+    },
+    {
+        name: 'Olga Kurylenko',
+        photo:
+            'https://vignette.wikia.nocookie.net/james-bond-007/images/f/fe/Olga_kurylenko.jpg/revision/latest?cb=20130928174353',
+        scores: [5, 1, 4, 4, 1, 2, 5, 4, 4]    
+    },
+    {
+        name: 'Olga Kurylenko',
+        photo:
+            'https://vignette.wikia.nocookie.net/james-bond-007/images/f/fe/Olga_kurylenko.jpg/revision/latest?cb=20130928174353',
+        scores: [5, 1, 4, 4, 1, 2, 5, 4, 4]    
+    },
+    {
+        name: 'Olga Kurylenko',
+        photo:
+            'https://vignette.wikia.nocookie.net/james-bond-007/images/f/fe/Olga_kurylenko.jpg/revision/latest?cb=20130928174353',
+        scores: [5, 1, 4, 4, 1, 2, 5, 4, 4]    
+    },
+    {
+        name: 'Olga Kurylenko',
+        photo:
+            'https://vignette.wikia.nocookie.net/james-bond-007/images/f/fe/Olga_kurylenko.jpg/revision/latest?cb=20130928174353',
+        scores: [5, 1, 4, 4, 1, 2, 5, 4, 4]    
+    },
+    {
+        name: 'Olga Kurylenko',
+        photo:
+            'https://vignette.wikia.nocookie.net/james-bond-007/images/f/fe/Olga_kurylenko.jpg/revision/latest?cb=20130928174353',
+        scores: [5, 1, 4, 4, 1, 2, 5, 4, 4]    
+    },
+    {
+        name: 'Olga Kurylenko',
+        photo:
+            'https://vignette.wikia.nocookie.net/james-bond-007/images/f/fe/Olga_kurylenko.jpg/revision/latest?cb=20130928174353',
+        scores: [5, 1, 4, 4, 1, 2, 5, 4, 4]    
+    },
+    {
+        name: 'Olga Kurylenko',
+        photo:
+            'https://vignette.wikia.nocookie.net/james-bond-007/images/f/fe/Olga_kurylenko.jpg/revision/latest?cb=20130928174353',
+        scores: [5, 1, 4, 4, 1, 2, 5, 4, 4]    
+    },
+];
+
+module.exports = friends;
---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var bodyPaser = require('body-parser');
+var path = require ('path');
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.use(bodyPaser.json());
+app.use(bodyPaser.urlencoded({ extended: true}));
+app.use(bodyPaser.text());
+app.use(bodyPaser.json({ type: 'application/vnd.api+json'}));
+
+app.use(express.static('app/public'));
+
+//if it does not work try without .js
+require('./app/routing/api-routes.js')(app);
+require('./app/routing/html-routes.js')(app);
+
+app.listen(port, () => console.log('listening on port %s', port));
